6ad5510da79a72a0ea0475968a712e92f077dbd7
Monika <Monika.cyberflax@gmail.com>
2022-09-26 20:42:37 +0530

    :commit

--- foundation/urls.py
+++ foundation/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('gallary/', include('gallary.urls')),
     path('shop/', include('shop.urls')),
     path('paypal/', include('paypal.standard.ipn.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)  +\
+              static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
