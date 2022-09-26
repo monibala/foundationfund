8678cbfdf5e02b564e63e262f47ff04de1eef735
Monika <Monika.cyberflax@gmail.com>
2022-09-26 13:38:01 +0530

    ch

--- foundation/settings.py
+++ foundation/settings.py
@@ -28,9 +28,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 # heroku deployment
-# import dj_database_url
-# db_from_env = dj_database_url.config(conn_max_age=500)
-# DatabaseError['default'].update(db_from_env)
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
 # Application definition
 
 INSTALLED_APPS = [
